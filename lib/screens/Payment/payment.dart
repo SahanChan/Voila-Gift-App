8844839103a48b5b3d9473bee8bc73de9f4afff2
sahan <xnscb8.chandrabahu@gmail.com>
2020-09-11 09:07:12 +0530

    pasanFinish

--- lib/screens/Payment/payment.dart
+++ lib/screens/Payment/payment.dart
@@ -2,6 +2,7 @@
 
 import 'package:VoilaGiftApp/constants.dart';
 import 'package:VoilaGiftApp/screens/OrderCart/voilaAppBar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -17,6 +18,11 @@ class _PaymentState extends State<Payment> {
   bool checkBoxVal1 = false;
   bool checkBoxVal2 = false;
   bool checkBoxVal3 = false;
+
+  final _formKey = GlobalKey<FormState>();
+  final cardNumber = TextEditingController();
+  final cvc = TextEditingController();
+  final expiryDate = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -109,15 +115,44 @@ class _PaymentState extends State<Payment> {
               ],
             ),
             Form(
+              key: _formKey,
               child: Padding(
                 padding: const EdgeInsets.all(35.0),
                 child: Column(
                   children: <Widget>[
                     TextFormField(
-                        decoration: InputDecoration(hintText: 'Card Number')),
-                    TextFormField(decoration: InputDecoration(hintText: 'CVC')),
+                      controller: cardNumber,
+                      decoration: InputDecoration(hintText: 'Card Number'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Enter Card Number";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
                     TextFormField(
-                        decoration: InputDecoration(hintText: 'Expiry Date')),
+                      controller: cvc,
+                      decoration: InputDecoration(hintText: 'CVC'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Enter CVC";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    TextFormField(
+                      controller: expiryDate,
+                      decoration: InputDecoration(hintText: 'Expiry Date'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return "Enter Expiry date";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
                   ],
                 ),
               ),
@@ -134,7 +169,23 @@ class _PaymentState extends State<Payment> {
               ),
             ),
             FlatButton(
-              onPressed: () {},
+              onPressed: () {
+                if (cardNumber.text.isNotEmpty &&
+                    cvc.text.isNotEmpty &&
+                    expiryDate.text.isNotEmpty) {
+                  Firestore.instance.collection('Payment').add({
+                    'cardNumber': cardNumber.text,
+                    'cvc': cvc.text,
+                    'expiryDate': expiryDate.text,
+                  }).then((_) {
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text('Successfully Added')));
+                    cardNumber.clear();
+                    cvc.clear();
+                    expiryDate.clear();
+                  });
+                }
+              },
               child: Text("Pay"),
               color: PrimaryColor,
             ),
