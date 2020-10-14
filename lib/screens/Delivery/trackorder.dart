c480b04bb8eb231f8cfe5413a062f98ca8c36451
HP <dhanukapps@gmail.com>
2020-10-14 13:01:34 +0530

    updates done on delivery

--- lib/screens/Delivery/trackorder.dart
+++ lib/screens/Delivery/trackorder.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:VoilaGiftApp/screens/Home/Home.dart';
 import 'package:VoilaGiftApp/screens/OrderCart/orderCart.dart';
 import 'package:provider/provider.dart';
-
+import 'package:provider/provider.dart';
 
 
 class trackOrderPage extends StatefulWidget {
